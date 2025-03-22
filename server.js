a93bed939575ebc68a56e835b675953f5b2445c9
DEEPAK SAGAR <sagardeepak2002@gmail.com>
2025-03-22 12:08:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const Calculator = require('./calculator');
 const app = express();
 const calculator = new Calculator();
 
+// Disable Express 'X-Powered-By' header
+app.disable('x-powered-by');
+
+// Serve static files (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
 
+// API Endpoints
 app.get('/add', (req, res) => {
     const { a, b } = req.query;
     res.json({ result: calculator.add(Number(a), Number(b)) });
@@ -31,12 +36,8 @@ app.get('/divide', (req, res) => {
     }
 });
 
-// Start the server only if not in a test environment
-if (require.main === module) {
-    const PORT = 3000;
-    app.listen(PORT, '0.0.0.0', () => {
-        console.log(`Server running at http://0.0.0.0:${PORT}`);
-    });
-}
-
-module.exports = app;
+// Start the server
+const PORT = 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${PORT}`);
+});
